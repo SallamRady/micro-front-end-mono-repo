b6151856cd337be46b3c793bff70c65da4312117
SallamRady <sallamrady@gmail.com>
2024-10-31 22:55:37 +0300

    upload file change

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarkettingApp from "./components/MarkettingApp";
 export default () => {
   return (
     <div>
-      <h1>Container App</h1>
+      <h1>Container App !</h1>
       <hr />
       <MarkettingApp />
     </div>
